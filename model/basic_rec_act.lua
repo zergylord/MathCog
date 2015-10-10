54d107d01272202f7eee88ae7bba3ba22961f702
Steven Hansen <zergylord@gmail.com>
2015-10-09 23:49:01 -0700

    recurrent connections no longer dependent on number of action factors

--- model/basic_rec_act.lua
+++ model/basic_rec_act.lua
@@ -6,17 +6,18 @@
 require 'nngraph'
 require 'nn'
 local model = {} 
-local num_dim,num_actions
+local num_dim,num_actions,act_factors
 local num_hid = 7
 function model.create(n_d,n_act)
     num_dim = n_d
     num_actions = n_act
+    act_factors = (#num_actions)[1] 
     local input = nn.Identity()()
     local last_hid = nn.Identity()()
     local pools = {}
     table.insert(pools,input)
     table.insert(pools,last_hid)
-    for a=1,(#num_actions)[1] do
+    for a=1,act_factors do
         table.insert(pools,nn.Identity()())
     end
 
@@ -24,7 +25,7 @@ function model.create(n_d,n_act)
     local hid = nn.ReLU()(nn.Linear(num_dim+num_hid+num_actions:sum(),num_hid)(total_input))
 
     local out_pools = {}
-    for a=1,(#num_actions)[1] do
+    for a=1,act_factors do
         table.insert(out_pools,nn.LogSoftMax()(nn.Linear(num_hid,num_actions[a])(hid)))
     end
     table.insert(out_pools,hid)
@@ -35,21 +36,21 @@ end
 local nll_crit = nn.ClassNLLCriterion()
 function model.prep_rec_state(mb_size,outputs,actions)
     local act_vec = {}
-    for a = 1,(#num_actions)[1] do
+    for a = 1,act_factors do
         act_vec[a] = torch.zeros(mb_size,num_actions[a])
     end
     if not outputs then --init rec state
         table.insert(act_vec,1,torch.zeros(mb_size,num_hid))
         return act_vec
     else
-        for a = 1,(#num_actions)[1] do
+        for a = 1,act_factors do
             if mb_size > 1 then
                 act_vec[a]:scatter(2,actions[a][{{1,mb_size},{}}]:long(),1)
             else
                 act_vec[a][1][actions[a][1][1]] = 1
             end
         end
-        table.insert(act_vec,1,outputs[3][{{1,mb_size},{}}])
+        table.insert(act_vec,1,outputs[act_factors+1][{{1,mb_size},{}}])
         return act_vec
     end
 end
@@ -57,21 +58,21 @@ end
 function model.prep_grads(mb_size,outputs,actions,R,prev_grads)
     local grad = {}
     local loss = 0
-    for a =1,(#num_actions)[1] do
+    for a =1,act_factors do
         loss = loss + nll_crit:forward(outputs[a],actions[a][{{},1}])
         grad[a] = nll_crit:backward(outputs[a],actions[a][{{},1}]):clone()
         grad[a]:cmul(R:repeatTensor(1,grad[a]:size()[2]))
     end
-    grad[3] = torch.zeros(mb_size,num_hid)
+    grad[act_factors+1] = torch.zeros(mb_size,num_hid)
     if prev_grads then
-        grad[3][{{1,prev_grads[2]:size()[1]},{}}] = prev_grads[2]
+        grad[act_factors+1][{{1,prev_grads[2]:size()[1]},{}}] = prev_grads[2]
     end
     return loss,grad
 end
 
 function model.sample_actions(outputs)
     actions = {}
-    for a = 1,(#num_actions)[1] do
+    for a = 1,act_factors do
         actions[a] = torch.multinomial(torch.exp(outputs[a]),1)
     end
     return actions
